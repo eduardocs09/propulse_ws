009eab0a8dcafb81f23ebe1463089e968a84d097
Eduardo <eduardocs09@gmail.com>
2017-09-01 21:10:17 -0300

    Adding new image files and finishing the Home page

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+  $(window).scroll(function () {
+        if ($(window).scrollTop() > 500) {
+            $("#navigation").css("background-color","black");
+        } else {
+            $("#navigation").css("background-color", "transparent");
+        }
+    });
+
+});
